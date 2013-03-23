282de99aeec907bc5cbb3c6287cedf81c248e34e
Lautaro Petaccio <lausuper@gmail.com>
2013-03-23 16:19:27 -0300

    Ahora se crea un arreglo de doubles para guardar los datos ya que se necesitan en otras funciones

--- metnumTP1.cpp
+++ metnumTP1.cpp
@@ -14,23 +14,27 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	ifstream file(argv[1]);
+	double *dataArr;
 	if(file.is_open()) {
 		double sumatoria=0;
 		double sumatoriaConLog=0;
 		float actualFloat = 0;
 		unsigned int size;
 		file >> size;
+		dataArr = new double[size];
 		printf("Size is: %d \n", size);
 		for(unsigned int i=0;i<size;i++) {
 			file >> actualFloat;
-			sumatoria += actualFloat;
-			sumatoriaConLog += actualFloat*log(actualFloat);
+			dataArr[i] = actualFloat;
+			sumatoria += dataArr[i];
+			sumatoriaConLog += dataArr[i]*log(dataArr[i]);
 		}
 		sumatoria /= size;
 		sumatoriaConLog /= size;
 		cout << "Sumatoria: " << sumatoria << endl;
 		cout << "SumatoriaConLog: " << sumatoriaConLog << endl;
 		file.close();
+		delete dataArr;
 	}
 	else {
 		cout << "File not oppened" << endl;
