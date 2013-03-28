97611895604219e62b3ebb4ea3c55830c9ef2bb2
Lautaro Petaccio <lausuper@gmail.com>
2013-03-28 12:21:00 -0300

    Agregadas las funciones derivadas de M y MSombrero y la funcion despejada para estimar beta

--- src/TP1.cpp
+++ src/TP1.cpp
@@ -19,13 +19,37 @@ TFloat f(const TFloat &x) { //Funcion ilustrativa
        return result;
 }
 
+TFloat funcionDespeje(const TFloat &beta, const int precision=52) {
+	//Asumimos que log es ln, hay que ver eso
+	TFloat result(log(1 + (beta*(R(beta)-R(0))).dbl()) + 2*log(M(beta).dbl()) - log(M(2*beta).dbl()), precision);
+	return result;
+}
+
+TFloat MDif(const TFloat &beta, const int precision=52) { //Se utiliza regla de la cadena para derivar, ver  si está bien
+	//sum(xi^beta) = beta*sum(xi^beta-1)/n =
+	TFloat result(0.0, precision);
+	for(unsigned int i=0;i<dataSize;i++) {
+		result = beta*pow(dataArr[i], beta.dbl()-1); //Ver que onda con la precision en las potencias
+	}
+	return result*(1/dataSize);
+}
+
 TFloat M(const TFloat &beta, const int precision=52) { //Beta vendria a ser S
 	//beta es TFloat porque es el parametro a aproximar
 	TFloat result(0.0, precision);
 	for(unsigned int i=0;i<dataSize;i++) {
-		result+pow(dataArr[i], beta.dbl()); //Ver que onda con la precision en las potencias
+		result+pow(dataArr[i], beta.dbl()-1); //Ver que onda con la precision en las potencias
 	}
-	return result*(1/dataSize);
+	return result*(beta/dataSize);
+}
+
+TFloat mSombreroDif(const TFloat &beta, const int precision=52) { //Ver si está bien
+	TFloat result(0.0, precision);
+	//la derivada de sum(x^beta*log(x)) = sum(x^(beta-1)*(beta*log(x)+1)))*beta
+	for(unsigned int i=0;i<dataSize;i++) {
+		result+((pow(dataArr[i], beta.dbl()-1)*(beta*log(dataArr[i]))+1)); //Ver si es log o ln
+	}
+	return result*(beta/dataSize);
 }
 
 TFloat mSombrero(const TFloat &beta, const int precision=52) {
@@ -39,6 +63,22 @@ TFloat mSombrero(const TFloat &beta, const int precision=52) {
 
 TFloat R(const TFloat &beta,const int precision=52) {
 	return mSombrero(beta)/mSombrero(beta);
+}
+
+TFloat newton(const double x0, const double tol, const int maxIter, const int precision=52) {
+	int noIter = 0;
+	TFloat x0TF(tol, precision);
+	TFloat tolTF(tol, precision);
+	TFloat pTF(tol, precision);
+	cout <<"It\t"<<"X0\t"<<"F(X0)\t"<<"FDif(X0)\t"<<"P"<<endl;
+	do {
+		//pTF = x0TF-(funcionDespeje(x0TF)/fDif(x0TF)); vamos a necesitar la derivada de lo que usemos, hay que ver como se derivan
+		x0TF = pTF;
+		cout <<noIter<<"\t"<<x0TF.dbl()<<"\t"<<f(x0TF).dbl()<<"\t"<</*fDif.dbl()*/"Nada"<<"\t"<<pTF.dbl()<<endl;
+		noIter++;
+	}
+	while((abs((pTF-x0TF).dbl())/abs(pTF.dbl())<tolTF.dbl())&&(noIter<maxIter)); //Cota de error |pn - pn-1|/|pn|
+	return pTF;
 }
 
 TFloat biseccion(const double a, const double b, const double tol, const int maxIter, const int precision=52) {
