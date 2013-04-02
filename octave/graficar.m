25f19bdc014706d3d918a3ea8b1a8d0db0786f97
Leandro Lovisolo <leandro@leandro.me>
2013-04-02 07:58:42 -0300

    Precisión en etiquetas de los gráficos

--- octave/graficar.m
+++ octave/graficar.m
@@ -1,15 +1,39 @@
 #!/usr/bin/octave -qf
 
 source("octave/leer_datos.m")
-source("octave/dibujarHistyAjuste.m")
 source("octave/GGDpdf_c.m")
 
-args = argv();
-d = leer_datos(args{2});
+args   = argv();
+data   = leer_datos(args{2});
+sigma  = args{3};
+beta   = args{4};
+lambda = args{5};
 
-dibujarHistyAjuste(d, str2double(args{3}),
-                      str2double(args{4}),
-                      str2double(args{5}))
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Gráfico - Inicio                                                          %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+x_bins = min(data) : 0.02 : max(data);
+
+% cuento cant repeticiones en rangos delimitados por x_bins
+count = histc(data,x_bins);
+
+figure;
+color = [0, 0.7, 0];
+bar(x_bins, count / sum(count), "facecolor", color, "edgecolor", color);
+
+hold on
+y = GGDpdf_c(x_bins, str2double(sigma), str2double(beta), str2double(lambda));
+p = plot(x_bins, y/sum(y), 'r');
+set(p, "linewidth", 5)
+
+title(strcat("$\\sigma = ",  sigma,  "$ \\hspace{2.5mm}",
+	         "$\\beta = ",   beta,   "$ \\hspace{2.5mm}",
+	         "$\\lambda = ", lambda, "$"));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Gráfico - Fin                                                             %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if strcmp(args{1}, "")
 	printf("Presionar cualquier tecla para terminar.\n")
