586378ecbc1b6460631da625fc38ddff8053ddb1
Leandro Lovisolo <leandro@leandro.me>
2013-04-13 05:08:04 -0300

    Formatting

--- src/Main.cpp
+++ src/Main.cpp
@@ -143,12 +143,12 @@ int main(int argc, char *argv[]) {
 
     // Mostramos resultados en pantalla
     if(args >> OptionPresent("csv")) {
-        cout << path << ","
-             << Sigma(beta.first, *muestra, t) << ","
-             << beta.first << ","
+        cout << path                            << ","
+             << Sigma(beta.first, *muestra, t)  << ","
+             << beta.first                      << ","
              << Lambda(beta.first, *muestra, t) << ","
-             << beta.second << ","
-             << (c * 1000 / CLOCKS_PER_SEC) << endl;
+             << beta.second                     << ","
+             << (c * 1000 / CLOCKS_PER_SEC)     << endl;
     } else {
         cout << "              Sigma = " << Sigma(beta.first, *muestra, t) << endl
              << "               Beta = " << beta.first << endl
