f61c88ab382dd955931a1613c7d5a16da8835a4e
Leandro Lovisolo <leandro@leandro.me>
2013-04-14 03:33:07 -0300

    Soporte para graficar varias curvas de ajuste sobre un mismo histograma

--- octave/graficar.m
+++ octave/graficar.m
@@ -5,9 +5,18 @@ source("octave/GGDpdf_c.m")
 
 args   = argv();
 data   = leer_datos(args{2});
-sigma  = args{3};
-beta   = args{4};
-lambda = args{5};
+sigma  = [];
+beta   = [];
+lambda = [];
+colors = {"red", "blue", "magenta", "black"};
+
+i = 3;
+while(i < length(args))
+    sigma(end+1)  = str2double(args{i});
+    beta(end+1)   = str2double(args{i+1});
+    lambda(end+1) = str2double(args{i+2});
+    i += 3;
+endwhile
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Gráfico - Inicio                                                          %%
@@ -22,23 +31,30 @@ figure;
 color = [0, 0.7, 0];
 bar(x_bins, count / sum(count), "facecolor", color, "edgecolor", color);
 
-hold on
-y = GGDpdf_c(x_bins, str2double(sigma), str2double(beta), str2double(lambda));
-p = plot(x_bins, y/sum(y), 'r');
-set(p, "linewidth", 5)
+i = 1;
+while(i <= length(sigma))
+    hold on
+    y = GGDpdf_c(x_bins, sigma(i), beta(i), lambda(i));
+    p = plot(x_bins, y/sum(y), 'r');
+    set(p, "linewidth", 3)
+    set(p, "color", colors{i})
+    i++;
+endwhile
 
-title(strcat("$\\sigma = ",  sigma,  "$ \\hspace{2.5mm}",
-	         "$\\beta = ",   beta,   "$ \\hspace{2.5mm}",
-	         "$\\lambda = ", lambda, "$"));
+% Pendiente: mostrar etiquetas coloreadas en el gráfico cuando se tienen
+% más de una curva de ajuste.
+title(strcat("$\\sigma = ",  sigma(1),  "$ \\hspace{2.5mm}",
+             "$\\beta = ",   beta(1),   "$ \\hspace{2.5mm}",
+             "$\\lambda = ", lambda(1), "$"));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Gráfico - Fin                                                             %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if strcmp(args{1}, "")
-	printf("Presionar cualquier tecla para terminar.\n")
-	pause	
+    printf("Presionar cualquier tecla para terminar.\n")
+    pause   
 else
-	print(args{1}, "-S400,300", "-dtex")
-	printf(cstrcat("El gráfico ", args{1}, " fue generado exitosamente.\n"))
+    print(args{1}, "-S400,300", "-dtex")
+    printf(cstrcat("El gráfico ", args{1}, " fue generado exitosamente.\n"))
 endif
