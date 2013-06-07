6b8f111b07396f6975f678188cea62d720768292
Lautaro Petaccio <lausuper@gmail.com>
2013-06-07 12:28:29 -0300

    Pequeña modificación al script para graficar la ecuacion4

--- octave/graficarEcuacion4.m
+++ octave/graficarEcuacion4.m
@@ -14,8 +14,9 @@ puntos4 = [];
 puntos5 = [];
 puntos6 = [];
 puntos7 = [];
+%puntos8 = [];
 
-for i=0:0.5:100
+for i=-5:0.5:100
 	puntos1(end + 1) = ecuacion4(i, datos1);
 	puntos2(end + 1) = ecuacion4(i, datos2);
 	puntos3(end + 1) = ecuacion4(i, datos3);
@@ -23,9 +24,11 @@ for i=0:0.5:100
 	puntos5(end + 1) = ecuacion4(i, datos5);
 	puntos6(end + 1) = ecuacion4(i, datos6);
 	puntos7(end + 1) = ecuacion4(i, datos7);
+%	puntos8(end + 1) = 0;
 endfor
 figure;
 hold on;
+grid on;
 plot(puntos1, 'Color', 'red');
 plot(puntos2, 'Color', 'magenta');
 plot(puntos3, 'Color', 'blue');
@@ -33,4 +36,5 @@ plot(puntos4, 'Color', 'green');
 plot(puntos5, 'Color', 'yellow');
 plot(puntos6, 'Color', 'black');
 plot(puntos7, 'Color', 'cyan');
-legend('Muestra X1','Muestra X2', 'Muestra X3', 'Muestra X4', 'Muestra X5', 'Muestra X6', 'Muestra X7');
+%plot(puntos8, 'Color', 'brown');
+legend('Muestra X1','Muestra X2', 'Muestra X3', 'Muestra X4', 'Muestra X5', 'Muestra X6', 'Muestra X7', 'Location','SouthEast');
